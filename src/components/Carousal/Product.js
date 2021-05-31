ce615fd4c08dff8a07b91880ecae7985848d2f52
Sarvar <sarvar.kumar@weareflamingo.in>
2021-05-31 19:39:48 +0530

    fix linting error

--- src/components/Carousal/Product.js
+++ src/components/Carousal/Product.js
@@ -1,12 +1,12 @@
 import Item from './Item';
 
-function Product({ index, item }) {
+function Product({ index }) {
   let items = [1, 2, 3, 4];
   return (
     <div className={`carousel-item${index === 0 ? ' active' : ''}`}>
       <div className="deals_main">
         {items.map((item, index) => {
-          return <Item key={index} />;
+          return <Item key={index} item={item} />;
         })}
       </div>
     </div>
