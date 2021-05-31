c8996a1f15df010041a717e6f4b3c2102343b3d6
Sarvar <sarvar.kumar@weareflamingo.in>
2021-05-31 21:15:26 +0530

    completed footer

--- src/shared/Footer.js
+++ src/shared/Footer.js
@@ -2,6 +2,7 @@ import facebook from '../assets/images/facebook.svg';
 import linkedin from '../assets/images/linkedin.svg';
 import twitter from '../assets/images/twitter.svg';
 import youtube from '../assets/images/youtube.svg';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
@@ -9,53 +10,56 @@ function Footer() {
       <div className="container">
         <div className="footer_main">
           <div className="footer_item">
-            <span className="logo">AnyShop</span>
+            <span className="logo">{process.env.REACT_APP_NAME}</span>
           </div>
           <div className="footer_item">
             <span className="f_link_header">Quick Links</span>
             <ul>
-              <a href="/">Home</a>
+              <Link to="/">Home</Link>
               <a href="/sitemap">Sitemap</a>
             </ul>
           </div>
           <div className="footer_item">
             <span className="f_link_header">Company</span>
             <ul>
-              <a href="/about-us">About Us</a>
-              <a href="/contact-us">Contact Us</a>
+              <Link to="/about-us">About Us</Link>
+              <Link to="/contact-us">Contact Us</Link>
             </ul>
           </div>
           <div className="footer_item">
             <span className="f_link_header">Policies</span>
             <ul>
-              <a href="/privacy-policy">Privacy Policy</a>
-              <a href="/refund-policy">Refund Policy</a>
+              <Link to="/privacy-policy">Privacy Policy</Link>
+              <Link to="/refund-policy">Refund Policy</Link>
             </ul>
           </div>
         </div>
         <div className="copyRight">
           <div className="copyRight_body">
-            <span>© AnyShop. 2021. All Rights Reserved.</span>
+            <span>
+              ©<Link to="/privacy-policy">AnyShop</Link>{' '}
+              {new Date().getFullYear()}. All Rights Reserved.
+            </span>
             <div className="social_media">
               <span>Follow us:</span>
               <ul>
                 <li>
-                  <a href="#">
+                  <a href="https://www.facebook.com">
                     <img src={facebook} alt="" />
                   </a>
                 </li>
                 <li>
-                  <a href="#">
+                  <a href="https://www.linkedin.com">
                     <img src={linkedin} alt="" />
                   </a>
                 </li>
                 <li>
-                  <a href="#">
+                  <a href="https://twitter.com">
                     <img src={twitter} alt="" />
                   </a>
                 </li>
                 <li>
-                  <a href="#">
+                  <a href="https://www.youtube.com">
                     <img src={youtube} alt="" />
                   </a>
                 </li>
