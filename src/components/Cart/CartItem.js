9c8b6f2e72c64b4ea64daa04970737517629edb3
Sarvar <sarvar.kumar@weareflamingo.in>
2021-06-01 13:11:40 +0530

    added cart functionality

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,21 +1,28 @@
 import plus from '../../assets/images/plus.svg';
 import minus from '../../assets/images/minus.svg';
-import product_image from '../../assets/images/product-1.jpeg';
+// import product_image from '../../assets/images/product-1.jpeg';
 
-function CartItem({ item }) {
+function CartItem({ item, handleUpdateCartQty, handleRemoveFromCart }) {
+  console.log(item);
   return (
     <div className="shopping_body">
       <div className="product_items">
         <div className="item_img_btn">
           <div className="item_img">
-            <img src={product_image} className="img-fluid" alt="cartImg" />
+            <img src={item.media.source} className="img-fluid" alt="cartImg" />
           </div>
           <div className="item_add_btn">
-            <span className="minus">
+            <span
+              className="minus"
+              onClick={() => handleUpdateCartQty(item.id, item.quantity - 1)}
+            >
               <img src={minus} alt="" />
             </span>
             <span className="dynamic_text">{item.quantity}</span>
-            <span className="plus">
+            <span
+              className="plus"
+              onClick={() => handleUpdateCartQty(item.id, item.quantity + 1)}
+            >
               <img src={plus} alt="" />
             </span>
           </div>
@@ -27,19 +34,28 @@ function CartItem({ item }) {
           </span>
           <span className="item_desc">can span multiple lines</span>
           <div className="price_sec">
-            <span className="fixed_price">₹{item.price}</span>
-            <span className="price">₹{item.actualPrice}</span>
-            <span className="percent_off_price">33% Off</span>
+            <span className="fixed_price">
+              {item.price.formatted_with_symbol}
+            </span>
+            <span className="percent_off_price">
+              {item.line_total.formatted_with_symbol}
+            </span>
           </div>
           <div className="remove_items">
-            <a href="#">Remove</a>
+            <a
+              onClick={() => {
+                handleRemoveFromCart(item.id);
+              }}
+            >
+              Remove
+            </a>
             <a href="#">Move to Wishlist</a>
           </div>
         </div>
       </div>
       <div className="delivery_date">
         <span className="delivey_desc">Expected Delivery</span>
-        <span className="cnf_date">Tomorrow</span>
+        <span className="cnf_date">3 days</span>
       </div>
     </div>
   );
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,32 +1,97 @@
+import heart from '../assets/images/heart.svg';
+import share from '../assets/images/share.svg';
+import trash from '../assets/images/trash.svg';
+
 import CartItem from '../components/Cart/CartItem';
 import '../assets/styles/cart.css';
+import { commerce } from '../lib/commerce';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 function Cart() {
-  let cart = {
-    items: [
-      {
-        productId: 1,
-        name: 'Test Product 1',
-        price: '999.99',
-        actualPrice: '1299.99',
-        quantity: 2,
-      },
-      {
-        productId: 2,
-        name: 'Test Product 2',
-        price: '499.99',
-        actualPrice: '649.99',
-        quantity: 1,
-      },
-      {
-        productId: 3,
-        name: 'Test Product 3',
-        price: '256',
-        actualPrice: '345',
-        quantity: 1,
-      },
-    ],
+  //   let cart = {
+  //     items: [
+  //       {
+  //         productId: 1,
+  //         name: 'Test Product 1',
+  //         price: '999.99',
+  //         actualPrice: '1299.99',
+  //         quantity: 2,
+  //       },
+  //       {
+  //         productId: 2,
+  //         name: 'Test Product 2',
+  //         price: '499.99',
+  //         actualPrice: '649.99',
+  //         quantity: 1,
+  //       },
+  //       {
+  //         productId: 3,
+  //         name: 'Test Product 3',
+  //         price: '256',
+  //         actualPrice: '345',
+  //         quantity: 1,
+  //       },
+  //     ],
+  //   };
+  const [cart, setCart] = useState({
+    total_unique_items: 0,
+    subtotal: {
+      formatted_with_symbol: '$0',
+    },
+  });
+  const [fetching, setFetching] = useState(true);
+
+  const handleUpdateCartQty = (lineItemId, quantity) => {
+    return commerce.cart
+      .update(lineItemId, { quantity })
+      .then((resp) => {
+        console.log(resp.cart);
+        setCart(resp.cart);
+      })
+      .catch((error) => {
+        console.log('There was an error updating the cart items', error);
+      });
   };
+
+  const handleRemoveFromCart = (lineItemId) => {
+    return commerce.cart
+      .remove(lineItemId)
+      .then((resp) => {
+        setCart(resp.cart);
+      })
+      .catch((error) => {
+        console.error(
+          'There was an error removing the item from the cart',
+          error,
+        );
+      });
+  };
+
+  const handleEmptyCart = async () => {
+    return await commerce.cart
+      .empty()
+      .then((resp) => {
+        setCart(resp.cart);
+      })
+      .catch((error) => {
+        console.error('There was an error emptying the cart', error);
+      });
+  };
+  useEffect(async () => {
+    setCart(
+      await fetchCart()
+        .then((cart) => {
+          console.log(cart);
+          setFetching(false);
+          return cart;
+        })
+        .catch((error) => {
+          console.log(error);
+          setFetching(false);
+        }),
+    );
+  }, []);
   return (
     <section className="shopping_cart">
       <div className="container">
@@ -35,26 +100,50 @@ function Cart() {
             <div className="product_details_header">
               <h2 className="second_heading">
                 Shopping Cart (
-                {cart.items.length === 1
+                {cart.total_items === 1
                   ? '1 item'
-                  : `${cart.items.length} items`}
+                  : `${cart.total_unique_items} items`}
                 )
               </h2>
               <div className="details_in_right">
                 <span>
-                  <img src="/images/heart.svg" alt="" />
+                  <img src={heart} alt="heart" />
                 </span>
                 <span>
-                  <img src="/images/share.svg" alt="" />
+                  <img src={share} alt="share" />
                 </span>
-                <span>
-                  <img src="/images/trash.svg" alt="" />
+                <span
+                  onClick={() => {
+                    handleEmptyCart();
+                  }}
+                >
+                  <img src={trash} alt="trash" />
                 </span>
               </div>
             </div>
-            {cart.items.map((item) => {
-              return <CartItem key={item.productId} item={item} />;
-            })}
+            {fetching ? (
+              <p>Fetching Cart Details</p>
+            ) : cart.total_items ? (
+              cart.line_items.map((item) => {
+                return (
+                  <CartItem
+                    key={item.id}
+                    item={item}
+                    handleUpdateCartQty={handleUpdateCartQty}
+                    handleRemoveFromCart={handleRemoveFromCart}
+                  />
+                );
+              })
+            ) : (
+              <>
+                <p>Your cart is empty</p>
+                <Link to={'/'}>
+                  <button className="shopping_btn Continue">
+                    Continue Shopping
+                  </button>
+                </Link>
+              </>
+            )}
           </div>
 
           <div className="price_details">
@@ -64,34 +153,33 @@ function Cart() {
             <div className="price_d_body">
               <ul>
                 <li>Price</li>
-                <li>₹4499.97</li>
+                <li>{cart.subtotal.formatted_with_symbol}</li>
               </ul>
               <ul>
                 <li>Discount</li>
-                <li>₹1500</li>
+                <li>Calculated at checkout</li>
               </ul>
               <ul>
                 <li>Delivery Charges</li>
-                <li className="free">FREE</li>
+                <li>Calculated at checkout</li>
               </ul>
             </div>
-            <div className="promo_code_sec">
-              <input type="text" placeholder="Enter Coupon Code" />
-              <button className="apply_btn">Apply</button>
-            </div>
+
             <div className="total_price_sec">
-              <span className="total_price">Total Price</span>
-              <span>₹2999.97</span>
+              <span className="total_price">Cart Value</span>
+              <span>{cart.subtotal.formatted_with_symbol}</span>
             </div>
             <div className="multi_btn">
-              <button className="shopping_btn Continue">
-                Continue Shopping
-              </button>
-              <a href="/checkout">
+              <Link to={'/'}>
+                <button className="shopping_btn Continue">
+                  Continue Shopping
+                </button>
+              </Link>
+              <Link to={'/checkout'}>
                 <button className="shopping_btn Proceed">
                   Proceed to Checkout
                 </button>
-              </a>
+              </Link>
             </div>
           </div>
         </div>
@@ -99,5 +187,11 @@ function Cart() {
     </section>
   );
 }
+
+const fetchCart = () => {
+  return commerce.cart.retrieve().then((cart) => {
+    return cart;
+  });
+};
 
 export default Cart;
